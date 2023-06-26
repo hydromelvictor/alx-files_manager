8fecc10d20d709767be63b40b0762c092300cd39
harkinder-dark <victorvaddely@gmail.com>
2023-06-26 11:30:53 +0000

    update

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const routes = express.Router();
 
+import AppController from '../controllers/AppController';
+
 routes.get('/status', AppController.getStatus);
 routes.get('stats', AppController.getStats);
+routes.post('/users', UsersController.postNew);
